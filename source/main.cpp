13e05c29dc0cf9a9b89b10cc3e2a4bffea0672f1
Alex <alexey.gerasev@gmail.com>
2015-10-08 15:28:13 +0600

    graphics handler

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,30 @@
 #include <media/media.hpp>
 
+class GraphicsHandler : public media::Graphics::Handler {
+public:
+	virtual void create() override {
+		logMessage("graphics created");
+	}
+	
+	virtual void destroy() override {
+		logMessage("graphics destroyed");
+	}
+	
+	virtual void resize(int w, int h) override {
+		logMessage("graphics resized %d %d", w, h);
+	}
+};
+
 class AppHandler : public media::App::Handler {
 public:
 	virtual void create() override {
+		super->getGraphics()->setHandler(new GraphicsHandler);
 		logMessage("app created");
 	}
 	
 	virtual void destroy() override {
+		delete super->getGraphics()->getHandler();
+		super->getGraphics()->setHandler(nullptr);
 		logMessage("app destroyed");
 	}
 } app_handler;
